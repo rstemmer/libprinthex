589ea796be6b10902729b2e5cd84a6a5f9904b6c
Ralf Stemmer <ralf.stemmer@gmx.net>
2020-05-22 19:15:49 +0200

    Build with clang if available

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-gcc -O2 -std=c99 -I. -c -o printhex.o printhex.c || exit 1
+if type "clang" > /dev/null 2>&1 ; then
+    CC=clang
+elif type "gcc" > /dev/null 2>&1 ; then
+    CC=gcc
+else
+    echo -e "\e[1;31mNo compiler found! Please install clang or gcc.\e[0m"
+    exit 1
+fi
+
+$CC -O2 -std=c99 -I. -c -o printhex.o printhex.c || exit 1
 
 rm libprinthex.a > /dev/null 2>&1
 ar qfs libprinthex.a printhex.o
